0accc114349d4baec9a882b0a003714f73b7e45a
arafatmollah <arafatshuvo18@gmail.com>
2024-01-29 19:14:11 +0600

    first commit

---
+++ src/Firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBUszEHGk6TfYIL2dCcczbwhuOv_BEeNDM",
+    authDomain: "final-project-fb6f2.firebaseapp.com",
+    projectId: "final-project-fb6f2",
+    storageBucket: "final-project-fb6f2.appspot.com",
+    messagingSenderId: "676150180129",
+    appId: "1:676150180129:web:e00d1126ec9860742e2660"
+  };
+
+  //init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  //init service
+  const projectFirestore = firebase.firestore()
+  const projectAuth = firebase.auth()
+
+  //timestamp
+  const timestamp = firebase.firestore.Timestamp
+
+  export {projectFirestore, projectAuth, timestamp}
---
+++ src/Pages/Signup/Signup.js
@@ -0,0 +1,11 @@
+import './Signup.css'
+
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
